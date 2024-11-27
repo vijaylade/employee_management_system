217944397d35ee9c32578d3cdc3fd50eb50b5cf6
vijaylade <vijaylade2000@gmail.in>
2024-11-27 20:11:31 +0530

    done the permissions crud

--- resources/views/Permission/permission.blade.php
+++ resources/views/Permission/permission.blade.php
@@ -24,21 +24,103 @@
                             <div class="row">
                                 <div class="col-6">
                                     <label for="name">Name</label>
-                                    <input type="text" name="name" id="name" class="form-control">
+                                    <input type="text" name="name" id="permissionname" class="form-control">
                                 </div>
 
                                 <div class="col-6">
                                     <label for="guard_name">Guard Name</label>
                                     <input type="text" name="guard_name" id="guard_name" class="form-control">
                                 </div>
+                            </div>
                         </form>
                     </div>
 
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary" id="permissionbtn">Save changes</button>
+                    <button type="submit" class="btn btn-primary" id="permissionbtn">Add Permission</button>
                 </div>
+            </div>
+        </div>
+    </div>
+
+
+    <table id="permissiontbl" class="display">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Gurad Name</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+
+    <div class="container mt-3">
+        {{-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+          Edit 
+        </button> --}}
+    </div>
+
+    
+    @if (session('success'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: '{{ session('success') }}',
+                position: 'top-end',
+                toast: true,
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true
+            });
+        </script>
+    @endif
+
+     <!-- The Modal -->
+     <div class="modal" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Edit Role</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+
+                    <div class="container">
+                        <form action="" id="permissionform" method="POST">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="row">
+                                <div class="col-6">
+                                    <label for="name">Name</label>
+                                    <input type="text" name="name" id="editpermissionname" class="form-control">
+                                </div>
+
+                                <div class="col-6">
+                                    <label for="guard_name">Guard Name</label>
+                                    <input type="text" name="guard_name" id="editguardpermission" class="form-control">
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </form>
+                    </div>
+
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                </div>
+
             </div>
         </div>
     </div>
